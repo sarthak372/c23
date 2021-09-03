f0e776f339d1e830e349bedfbc210ef2fe8cc1f4
sarthak372 <85297584+sarthak372@users.noreply.github.com>
2021-09-03 09:32:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,63 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+let engine;
+let world;
+
+var ground;
+var btn;
+var top_wall;
+
+function setup() {
+  createCanvas(400,400);
+
+  engine = Engine.create();
+  world = engine.world;
+ 
+  var ball_options={
+    restitution:0.95,
+  }
+  
+  btn=createImg("up.png");
+  btn.position(20,30);
+  btn.size(50,50);
+  btn.mouseClicked(vforce);
+  ground =new Ground(200,390,400,20);
+ 
+ top_wall = new Ground(50,200,30,20);
+  top_wall1 = new Ground(150,200,30,20);
+  top_wall2 = new Ground(250,200,30,20);
+  top_wall3 = new Ground(350,200,30,20);
+
+  ball=Bodies.circle(100,200,20,ball_options);
+  World.add(world,ball);
+
+  
+  
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+}
+
+
+function draw() 
+{
+  background(51);
+  
+  
+ellipse(ball.position.x,ball.position.y,20);
+  ground.show();
+  top_wall.show();
+  top_wall1.show();
+  top_wall2.show();
+  top_wall3.show();
+  Engine.update(engine);
+}
+
+function vforce() {
+  Matter.Body.applyForce(ball,{x:0,y:0},{x:0,y:-0.05});
+}
+
